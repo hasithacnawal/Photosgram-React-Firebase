01324ebf32bf78c922150e0af253f32166cfdc29
Chana96 <ahasithachandana@gmail.com>
2020-12-02 19:12:03 +0530

    2/12/2020

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,7 +1,23 @@
 import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
-function ImageGrid() {
-  return <div className="img-grid">images</div>;
+function ImageGrid({ setSelectedImg }) {
+  const { docs } = useFirestore("images");
+  console.log(docs);
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div
+            className="img-wrap"
+            key={doc.id}
+            onClick={() => setSelectedImg(doc.url)}
+          >
+            <img src={doc.url} alt="uploaded pic" />
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default ImageGrid;
